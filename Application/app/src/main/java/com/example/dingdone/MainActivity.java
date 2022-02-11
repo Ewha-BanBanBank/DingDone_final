64bbece1d0e399482ece355e3ddaa7e401c6f923
Eunseo Kim <70934572+eunseo22mv@users.noreply.github.com>
2022-02-11 14:37:29 +0900

    Add files via upload

---
+++ Application/app/src/main/java/com/example/dingdone/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.dingdone;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.dingdone.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
