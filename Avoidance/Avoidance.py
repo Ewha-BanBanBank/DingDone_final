9bb2725f663c49e129721c4510297f1179f0de85
JaeRyung Hong <61305409+Jar199@users.noreply.github.com>
2022-03-26 00:59:03 +0900

    Update Avoidance.py
    second version of Avoidance.py

--- Avoidance/Avoidance.py
+++ Avoidance/Avoidance.py
@@ -1,76 +1,67 @@
-from time import sleep
 import numpy as np
 
-
-class Avoidance:
-    def __init__(self, tello, depth, prevCase, angle, turn, speed=10):
-        # depth은 narray from np
-        self.me = tello
-        self.depth = depth
-        self.prevCase = prevCase
-        self.turn = turn
-        self.crit = 90 # critical of deciding to close distance
-        self.obs = self.depth <= self.crit
-        self.speed = speed
-        
-        # decide type of avoidance
-        self.obsType()
-
-        # compute angle to turn and the number of remained turn
-        #self.computeAngle(angle)
-
-        
-        #self.avoid()
+def detObstacle(depth_, crit):
+    obs = depth_ <= crit
+    return obs
+'''
+def doAvoid(obs, r_l, f_b, u_d, y):
+    yaw = 20
+    if obs[1, 2]:
+        y = -yaw
+        print("turn left")
+    else:
+        y = yaw
+        print("turn right")
     
-    def obsType(self):
-        idx = []
-        for i in range(len(self.obs)): idx.append(False not in self.obs)
-        if False in self.obs: # have no obstacle in video
-            self.isAvoid = False
-            self.Case = 0
+    if np.logical_and((sum(obs[:2, :2]) >= 4), (sum(obs[:2, 2]) <= 1)).any(): 
+        # Code of "(sum(obs[:2, :2]) >= 4) and (sum(obs[:2, 2]) <= 1):" makes ValueError: The truth value of an array with more than one element is ambiguous. Use a.any() or a.all()
+        #if obs[1,2] == True: u_d = 5
+        y = yaw
+        print("turn right")
+    elif np.logical_and((sum(obs[:2, 1:]) >= 4), (sum(obs[:2, 0]) <= 1)).any():
+        #if obs[1,0] == True: u_d = 5
+        y = -yaw
+        print("turn left")
+    elif sum(obs[1]) == 3:
+        print("turn right")
+        u_d = yaw
+    elif np.logical_and((sum(obs[:2,0]) == 2), (sum(obs[:2, 1:]) <= 2)).any():
+        y  = yaw
+        print("turn right")
+    elif np.logical_and((sum(obs[:2,2]) == 2), (sum(obs[:2, :2]) <= 2)).any():
+        y  = -yaw
+        print("turn left")
+    elif sum(obs[:2, 0]) == 0:
+        y = yaw
+        print("turn right")
+    elif sum(obs[:2, 2]) == 0:
+        y = -yaw
+        print("turn left")
+    elif sum(obs[:2, :]) == 1:
+        y = yaw
+        print("turn right")
+    else: y = yaw
+    
+    return r_l, f_b, u_d, y
+'''
 
-        elif True in idx: # when row that filled with obstance captured from video area exists more than one
-            self.isAvoid = True
-            self.Case = 1
-        
-        else: # when obstacle is captured from video area
-            self.isAvoid = True
-            self.Case = 2
-        return self.isAvoid
+def re_turn(yaw, target, f_b):
+    '''
+    if target == 0:
+        if
+        y = -(yaw - target) # -(yaw - target)/2
+    elif target == 180:
+        y = yaw - target # (yaw - target)/2
+    '''
+    y = target - yaw
+    if abs(y) >= 90:
+        y = int(y/2)
+    elif abs(y) >= 45:
+        y = int(y/3)
+    else:
+        y = int(y)
+    print("turn", y)
+    return f_b, y
 
-    def computeAngle(self, angle):
-        if self.Case == 1:
-            if (self.prevCase != self.Case) or (self.turn == 0):
-                self.angle = 80 # one of [60, 70, 80, 90]
-                self.turn = 1
 
-        elif self.Case == 2:
-            if (self.turn == 0) or (self.prevCase != self.Case):
-                s_angle = 45
-                self.angle = 90 if np.sum(self.obs) > 4 else s_angle # rotate diffent angle as size of obstacle 
-                self.turn = 2
 
-        else:
-            self.angle = angle
-            self.turn -= 1
-
-    def avoid(self, ): # doing avoidance on case type
-        if self.Case == 1:
-            if self.turn == 1: # rotate counter clockwise
-                self.me.send_fc_control(0, self.speed*2, 0, -(self.angle+10))
-
-            elif self.turn == 0: # rotate clockwise
-                self.me.send_fc_control(0, self.speed*2, 0, (self.angle+10)*2)
-
-        elif self.Case == 2:
-            if self.turn == 2:
-                self.me.send_fc_control(0, self.speed*2, 0, -(self.angle+10))
-
-            elif self.turn == 1:
-                self.me.send_fc_control(0, self.speed*2, 0, self.angle+10)
-                self.me.rotate_clockwise(2*self.angle)
-            
-            else: 
-                self.me.rotate_counter_clockwise(self.angle)
-        sleep(2)
-        return self.Case, self.angle, self.turn
